e2f3dc4c9fb0cabc97b19fe59283b48bfe4f2442
softmax0112p <softmax0112@proton.me>
2018-04-04 15:48:16 -0400

    Initial import

---
+++ index.js
@@ -0,0 +1,13 @@
+const SQRT2 = require( 'const-sqrt-two' );
+const todd_square = require('todd-square');
+
+/**
+ * HTTP Cloud Function.
+ *
+ * @param {Object} req Cloud Function request context.
+ * @param {Object} res Cloud Function response context.
+ */
+exports.helloGET = (req, res) => {
+  res.send('Hello World! The square root of 2 is ' + SQRT2 + 
+  ' or is it ' +todd_square(2) + '?');
+};
